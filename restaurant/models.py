1b1c9729e8366cf5931c71976f7dd7d2f3202c4e
wasihun-code <wasihunageru7947@gmail.com>
2023-12-23 17:49:26 +0530

    Models: Created Booking and Menu models Database: Changed default sqlite to MySQL database

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    n_guests = models.SmallIntegerField(default=0)
+    booking_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.name} - {self.n_guests} on {self.booking_date}"
+
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.title} - ${self.price}"
